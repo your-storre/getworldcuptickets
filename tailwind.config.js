72be62818365c0fc1479111e70a84c34a7687fd4
your-storre <babrianalee@gmail.com>
2026-05-28 14:33:53 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./app/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        primary: '#3B82F6',
-        secondary: '#1E40AF',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
